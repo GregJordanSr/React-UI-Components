2e5ff1e0aca82f9d23e9c5be20b2aa7c943299d3
Greg Jordan <gregljordan@gmail.com>
2019-04-22 16:21:17 -0400

    added the HeaderContainer

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Header.css';
+import './HeaderTitle';
+
+const HeaderContainer = () => {
+    return (
+        <div className = "header-container">
+        <ImageThumbnail />
+        <HeaderTitle />
+        </div>
+    );
+}
