559b770c9ef19ff4f06921fb719ccdd1a3561aa9
Greg Jordan <gregljordan@gmail.com>
2019-04-22 20:29:27 -0400

    added footer component

---
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import  './Footer.css';
+
+const Footer = () => {
+    return (
+        <div className= 'footer'>
+            <i className= 'icons' class="far fa-comment"></i>
+            <i className= 'icons' class="fas fa-retweet"></i>
+            <i className= 'icons' class="fal fa-heart"></i>
+            <i className= 'icons' class="fal fa-envelope"></i>
+        </div>
+    );
+}
+
+export default Footer;
