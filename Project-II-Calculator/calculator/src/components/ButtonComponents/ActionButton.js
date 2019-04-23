a15fa6070a36141ee5e25c1d4717e712d48e1285
Greg Jordan <gregljordan@gmail.com>
2019-04-23 16:32:29 -0400

    Components completed

--- Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,11 @@
 import React from 'react';
 import './Button.css';
+
+const actionButtons = props => {
+    return (
+        <div className="btn func" onClick={() => props.clickHandeler(props.func)}>
+            {props.func}
+        </div>
+    );
+};
+export default actionButtons;
--- Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,11 @@
 import React from 'react';
 import './Button.css';
+
+const numButtons = props => {
+    return (
+        <div className="btn num" onClick={() => props.clickHandler(props.number)}>
+            {props.number}
+        </div>
+    );
+};
+export default numButtons;
